52555fc896ddd4d85ce4908ccbe4a204f2f686eb
Hamza-Noah <hamza.nouh97@gmail.com>
2024-08-18 21:52:03 +0300

    added the footer component

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar/Navbar"
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 export default function Layout() {
   return (
     <>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+      <div className="pt-16">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </>
   );
 }

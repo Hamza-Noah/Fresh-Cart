8a79c85dd045ed58a8489f855d3d42eff8df40ad
Hamza-Noah <hamza.nouh97@gmail.com>
2024-08-25 04:59:57 +0300

    Fetch products and map to Product component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,3 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Product from "../Product/Product";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+
+  async function getData() {
+    let { data } = await axios.get(
+      "https://ecommerce.routemisr.com/api/v1/products"
+    );
+    setProducts(data.data);
+  }
+
+  console.log(products);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-3">
+        {products.map((product, i) => {
+          return <Product product={product} index={i} />;
+        })}
+      </div>
+    </>
+  );
 }

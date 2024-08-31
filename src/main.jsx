bd3998fd79756b79d7bf7958ed3bba749c093a9c
Hamza-Noah <hamza.nouh97@gmail.com>
2024-08-31 13:59:09 +0300

    added react slick library

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import App from "./App.jsx";
+import "./index.css";
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './index.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

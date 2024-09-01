3957ecb1d8530595147b18a965ac9e3abe181512
Hamza-Noah <hamza.nouh97@gmail.com>
2024-09-01 07:57:14 +0300

    added authorization to protect pages

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,3 +1,10 @@
-export default function ProtectedRoute() {
-  return <>ProtectedRoute</>;
+import { useContext } from "react";
+import {AuthContext} from "../../Contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({children}) {
+  const { userToken } = useContext(AuthContext);
+
+
+  return <>{userToken != null ? children : <Navigate to={"/login"}/>}</>;
 }

c838cb4675a99c0dc818deea31e80fd791fd6baa
Hamza-Noah <hamza.nouh97@gmail.com>
2024-09-02 10:35:33 +0300

    updated the structure of project by inserting the service in a services folder

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import RatingStars from "../RatingStars";
 import { AuthContext } from "../../Contexts/AuthContext";
-import addProductToCart from "../../cartService";
+import addProductToCart from "../../Services/cartService";
 
 export default function Product({ product }) {
   const { userToken } = useContext(AuthContext);
